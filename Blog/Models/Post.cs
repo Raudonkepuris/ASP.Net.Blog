ada91cbb400d6ac545682296a73a5d6d4eb76e81
Raudonkepuris <skamarakas.liutauras@protonmai.com>
2024-06-23 06:54:59 +0300

    Post and Tag models

---
+++ Blog/Models/Post.cs
@@ -0,0 +1,11 @@
+﻿namespace Blog.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Content { get; set; }
+        public string? Type { get; set; }
+        public List<Tag> Tags { get; set; } = [];
+    }
+}
