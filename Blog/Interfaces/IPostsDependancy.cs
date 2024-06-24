1cea4d90676f96859944c7426d110eddce39f5a2
Raudonkepuris <skamarakas.liutauras@protonmai.com>
2024-06-24 11:28:47 +0300

    Reapply "methods for admin panel"
    This reverts commit 62fa38cd8bfa628a75dbed61fdcf400d726d27cd.

---
+++ Blog/Interfaces/IPostsDependancy.cs
@@ -0,0 +1,11 @@
+﻿using Blog.Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Blog.Interfaces
+{
+    public interface IPostsDependancy
+    {
+        [Authorize]
+        Task<List<Post>> GetAllPosts();
+    }
+}
